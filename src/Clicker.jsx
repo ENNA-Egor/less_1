7d431998a5afa6eda5692c878def38ff0011f6a9
ENNA-Egor <e.yudakov@gmail.com>
2023-11-13 10:59:27 +0300

    Отформатировал

--- src/Clicker.jsx
+++ src/Clicker.jsx
@@ -6,25 +6,24 @@ class Clicker extends React.Component{
         count : 0
     };
 
+    handleClickPlus =()=>{
+        this.setState({count : this.state.count +1});
+    }
+    handleClickMinus =()=>{
+        this.setState({count : this.state.count -1});
+    }
 
-
-handleClickPlus =()=>{
-    this.setState({count : this.state.count +1});
-}
-handleClickMinus =()=>{
-    this.setState({count : this.state.count -1});
-}
-
-render() {
-    return(
-        <div className="clicker">
-            Hello React
-            <button className="btn" onClick={this.handleClickPlus}>+</button>
-            <input className="inp" type="text" value={this.state.count} />
-            <button className="btn" onClick={this.handleClickMinus}>-</button>
-        </div>
-     );
+    render() {
+        return(
+            <div className="clicker">
+                Hello React
+                <button className="btn" onClick={this.handleClickPlus}>+</button>
+                <input className="inp" type="text" value={this.state.count} />
+                <button className="btn" onClick={this.handleClickMinus}>-</button>
+            </div>
+        );
     }
 }
+
 export default  Clicker;
 
