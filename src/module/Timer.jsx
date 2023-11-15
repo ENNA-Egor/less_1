ac2d0fbbef3c8e3de0ffff11c32b66651fa4e068
ENNA-Egor <e.yudakov@gmail.com>
2023-11-15 09:19:08 +0300

    Скомпанован тфймер

--- src/module/Timer.jsx
+++ src/module/Timer.jsx
@@ -7,19 +7,44 @@ export default class Timer extends React.Component {
     };
 
     componentDidMount() {
-
+        const userCount = localStorage.getItem('timer')
+        if(userCount){
+            this.setState({count: +userCount})
+        }
     }
 
     componentDidUpdate() {
-        
+        localStorage.setItem('timer', this.state.count)
     }
 
-    componentWillUnmount() {}
+    componentWillUnmount() {
+        clearInterval(this.counterId)
+    }
+
+    handleStart=()=>{
+        this.setState({isCounting: true})
+
+        this.counterId = setInterval(() => {
+            this.setState({count: this.state.count +1})
+        }, 1000)
+    }
+
+    handleStop=()=> {
+        this.setState({isCounting: false})
+
+        clearInterval(this.counterId)
+    }
+
+    handleReset=()=> {
+        this.setState({isCounting: false , count: 0})
+        // this.setState({count: this.state.count =0})
+        clearInterval(this.counterId)
+    }
 
     render() {
         return (
             <div className="App">
-                <h1>React Timer</h1>
+                <h3>React Timer</h3>
                 <h3>{this.state.count}</h3>
                 {!this.state.isCounting ? (
                     <button onClick={this.handleStart}>Start</button>
