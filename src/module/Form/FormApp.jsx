5e0475a6458b310dc2af80f8520a2214854439dd
ENNA-Egor <e.yudakov@gmail.com>
2023-11-21 09:48:34 +0300

    Закончил с формой

--- src/module/Form/FormApp.jsx
+++ src/module/Form/FormApp.jsx
@@ -15,8 +15,32 @@ import React from 'react';
     }
 
     validateEmail=()=>{
-        if(this.state.email.length <3){
-            alert('No')
+        if(!/(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i.test(this.state.email.toLowerCase())){
+            alert('Invalid email')
+            this.setState({
+                email: '',
+                isAgreeWithTerms: false,
+            })
+
+        }
+    }
+
+    buttonOnClick =()=> {
+        let chesked = this.state.isAgreeWithTerms;
+        let mailed = this.state.email;
+        if (mailed == ''){
+            alert ('You must fill in your email address')
+            return
+        }
+        else if(chesked == false){
+            alert('you should accept all agreements');
+            return
+        }else {
+            alert('Thank you for your subsciption!')
+            this.setState({
+                email: '',
+                isAgreeWithTerms: false,
+            })
         }
     }
   
@@ -45,7 +69,10 @@ import React from 'react';
                     I agree with terms and conditions
                 </label>
                 <br />
-                <button>Send</button>
+                <button
+                    onClick={this.buttonOnClick}
+                    >Send
+                </button>
             </div>
         );
   }
