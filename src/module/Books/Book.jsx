a3c72faa4aed7d18bb7ecfe1c149e2f5ea8de296
ENNA-Egor <e.yudakov@gmail.com>
2024-01-05 14:14:17 +0300

    Добавил удаление  книги

--- src/module/Books/Book.jsx
+++ src/module/Books/Book.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import { CustomContext } from "../hooks/Context";
+
+import React, {useContext} from "react";
 
 const Book = (props) => {
+  const {removeBook} = useContext(CustomContext)
     return(
       <div className="book">
-      <h2>{props.name}</h2>
+      <h2 onClick={() => removeBook(props.id)} >{props.name}</h2>
       <p>{props.year}</p>
       <p>{props.price}</p>
       <h3>{props.rezume}</h3>
