e27f45af413c1f88dffa8d1c84483d677ea013fe
ENNA-Egor <e.yudakov@gmail.com>
2024-01-05 14:16:27 +0300

    Повесил событие удаления на карточку книги.

--- src/module/Books/Book.jsx
+++ src/module/Books/Book.jsx
@@ -5,8 +5,8 @@ import React, {useContext} from "react";
 const Book = (props) => {
   const {removeBook} = useContext(CustomContext)
     return(
-      <div className="book">
-      <h2 onClick={() => removeBook(props.id)} >{props.name}</h2>
+      <div className="book" onClick={() => removeBook(props.id)} >
+      <h2 >{props.name}</h2>
       <p>{props.year}</p>
       <p>{props.price}</p>
       <h3>{props.rezume}</h3>
